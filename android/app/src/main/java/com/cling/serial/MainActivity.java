3dd49433f100e7f359a42b29347276eae12a17c2
james kim <strive1024@gmail.com>
2024-09-12 11:28:29 +0900

    first commit

---
+++ android/app/src/main/java/com/cling/serial/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cling.serial;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type {CapacitorConfig} from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.cling.serial',
+  appName: 'serial',
+  webDir: 'out',
+}
+
+export default config
